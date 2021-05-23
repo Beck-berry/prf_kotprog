782358ce73be15d131c518105a06496687242364
Beck-berry <trebeka98@gmail.com>
2021-05-23 18:23:17 +0200

    init Kittens

--- node-api/database_init.js
+++ node-api/database_init.js
@@ -8,11 +8,29 @@ const dbNev = "prfdb"
 client.connect((err, db) => {
   if (err) throw err;
   console.log("Database connected!");
-  const collection = db.db(dbNev).collection("Users");
+  const uCollection = db.db(dbNev).collection("Users");
   const zoliUser = { username: "szaboz", password: bcrypt.hashSync("PRF2021", bcrypt.genSaltSync(10), null) };
-  collection.insertOne(zoliUser, function(err, res) {
+  uCollection.insertOne(zoliUser, function(err, res) {
     if (err) throw err;
     console.log("User added!");
+  });
+
+  const kCollection = db.db(dbNev).collection("Kittens");
+  let ujCicaList = [
+      { id: '1', name: 'Cukorfalat', desc: 'Legédesebb', price: '542', image: 'assets/cuki.jpg' },
+      { id: '2', name: 'Béla', desc: 'Legfélénkebb', price: '542', image: 'assets/bela.jpg' },
+      { id: '3', name: 'Rozi', desc: 'Legszebb', price: '542', image: 'assets/rozi.jpg' },
+      { id: '4', name: 'Szimbád', desc: 'Legbátrabb', price: '542', image: 'assets/szimbad.png' },
+      { id: '5', name: 'Garfield', desc: 'Legvörösebb', price: '542', image: 'assets/garfield.webp' },
+      { id: '6', name: 'Z', desc: 'Legvagányabb', price: '542', image: 'assets/z.png' },
+      { id: '7', name: 'Mancs', desc: 'Legzabálósabb', price: '542', image: 'assets/mancs.jpg' },
+      { id: '8', name: 'Picúr', desc: 'Legnyávogósabb', price: '542', image: 'assets/picur.jpg' },
+      { id: '9', name: 'Tigris', desc: 'Legkisebb', price: '542', image: 'assets/tigris.jpg' },
+      { id: '10', name: 'Maki', desc: 'Leggyorsabb', price: '542', image: 'assets/maki.jpeg' }
+  ];
+  kCollection.insertMany(ujCicaList, function(err, res) {
+    if (err) throw err;
+    console.log("Kittens added!");
   });
 });
 
